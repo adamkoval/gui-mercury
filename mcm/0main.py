beaa33af31df51d9cc0215bfdf8dd901104844ad
adamkoval <akoval@LAPTOP-M692C3QH.dundee.ac.uk>
2020-03-30 23:42:59 +0100

    Functionality to create instance of mercury and perform simulations on it, pipe the results to output dir and finally destroy instance of mercury.

--- mcm/0main.py
+++ mcm/0main.py
@@ -5,14 +5,15 @@ import sys
 import shutil
 from subprocess import Popen
 
-import func as fn
+import func as mcfn
 
-pyenv, bashenv = fn.read_envfile('envfile.txt')
+pyenv, bashenv, mercuryOG_path, rslts_path = mcfn.read_envfile("envfile.txt")
+if rslts_path.endswith("/"):
+    rslts_path = rslts_path[:-1]
+if mercuryOG_path.endswith("/"):
+    mercuryOG_path = mercuryOG_path[:-1]
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--resonance', '-res',
-                    dest='resonance',
-                    action='store')
 parser.add_argument('--no_runs', '-no',
                     dest='N_runs',
                     action='store')
@@ -21,41 +22,51 @@ parser.add_argument('--process_no', '-pno',
                     action='store')
 args = parser.parse_args()
 
-res_str = args.resonance
 N_runs = int(args.N_runs)
 pno = str(args.process_no)
 
 # Test for nominal resonance directory
-fn.make_directories(res_str)
+mcfn.make_rsltpath(rslts_path)
 
 # Count the number of files which exist already
-N_completed = fn.count_completed(res_str)
+n_completed = mcfn.count_completed(rslts_path)
+
+# Create instance of mercury
+m_inst = mcfn.mercury_instance(pno, mercuryOG_path)
+m_inst.create()
 
 # Main loop
 k = 0
 while k < N_runs:
-    print(' ~~~~~~~~~~~~~~~~~~~~~~~~\n',
-          '0main.py:\n',
-          'Run = {}\n'.format(k))
+    #print(" ~~~~~~~~~~~~~~~~~~~~~~~~\n",
+    #      "0main.py:\n",
+    #      "Run = {}\n".format(k))
 
-    # Randomizing input
-    p_randomize = Popen([pyenv, 'randomize.py', '-res', res_str, '-pno', pno])
-    p_randomize.wait()
+    ## Randomizing input
+    #p_randomize = Popen([pyenv, "randomize.py", "-res", res_str, "-pno", pno])
+    #p_randomize.wait()
 
     # Cleaning up old files from mercury dir
-    p_cleanup = Popen([bashenv, 'cleanup.sh', pno])
+    p_cleanup = Popen([bashenv, "cleanup.sh", pno])
     p_cleanup.wait()
 
-    # Execute and check if Mercury is running
-    p_check_mercury = Popen([bashenv, 'check_mercury.sh', pno])
-    p_check_mercury.wait()
-    N_completed = fn.count_completed(res_str)
-    print(' N_completed = {}\n'.format(N_completed))
-    print(' Mercury completed, copying files\n',
-          '~~~~~~~~~~~~~~~~~~~~~~~~\n')
+    # Execute Mercury
+    p_launch_mercury = Popen([bashenv, "launch_mercury.sh", pno])
+    p_launch_mercury.wait()
 
-    shutil.copyfile('mercury_{}/planet1.aei'.format(pno), '../completed/{}/planets/{}-planet1.aei'.format(res_str, N_completed))
-    shutil.copyfile('mercury_{}/planet2.aei'.format(pno), '../completed/{}/planets/{}-planet2.aei'.format(res_str, N_completed))
-    shutil.copyfile('mercury_{}/info.out'.format(pno), '../completed/{}/info/{}-info.out'.format(res_str, N_completed))
-    shutil.copyfile('mercury_{}/big.in'.format(pno), '../completed/{}/input/{}-big.in'.format(res_str, N_completed))
-    k += 1
+    n_completed = mcfn.count_completed(rslts_path)
+    print(" n_completed = {}\n".format(n_completed))
+    #print(" Mercury completed, copying files\n",
+    #      "~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+    shutil.copyfile("mercury_{}/xv.out".format(pno), "{}/outputs/{}-xv.out".format(rslts_path, n_completed))
+    shutil.copyfile("mercury_{}/ce.out".format(pno), "{}/outputs/{}-ce.out".format(rslts_path, n_completed))
+    shutil.copyfile("mercury_{}/info.out".format(pno), "{}/outputs/{}-info.out".format(rslts_path, n_completed))
+    shutil.copyfile("mercury_{}/big.in".format(pno), "{}/inputs/{}-big.in".format(rslts_path, n_completed))
+    shutil.copyfile("mercury_{}/small.in".format(pno), "{}/inputs/{}-small.in".format(rslts_path, n_completed))
+    shutil.copyfile("mercury_{}/param.in".format(pno), "{}/inputs/{}-param.in".format(rslts_path, n_completed))
+
+    k +=1
+
+# Destroy mercury instance
+m_inst.destroy()
