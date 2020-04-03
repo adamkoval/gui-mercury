7da940608f3f7537454292a84973e65564fd4815
adamkoval <akoval@LAPTOP-M692C3QH.dundee.ac.uk>
2020-04-03 01:44:29 +0100

    Create empty small.in or big.in file if 0 bodies are selected.

--- mcm/randomize.py
+++ mcm/randomize.py
@@ -31,43 +31,62 @@ bodyin = open("mercury_{}/{}.in".format(pno, btype), 'w')
 # Finding all body files
 body_files = [file for file in os.listdir("../gui/setup/") if file.startswith(btype) and file.endswith(".vals")]
 
-for file in body_files:
-    st = open("../gui/setup/{}".format(file), "r")
-    st = st.readlines()
-    for line in st:
-        exec(line)
+if len(body_files) != 0:
+    for file in body_files:
+        st = open("../gui/setup/{}".format(file), "r")
+        st = st.readlines()
+        for line in st:
+            exec(line)
 
+        if btype == "small":
+            if file == body_files[0]:
+                bodyin.write('\n'.join([
+                    ")O+_06 Small-body initial data  (WARNING: Do not delete this line!!)",
+                    ") Lines beginning with `)' are ignored.",
+                    ")---------------------------------------------------------------------",
+                    " style (Cartesian, Asteroidal, Cometary) = {}".format(coordinates),
+                    ")---------------------------------------------------------------------\n"]))
+            else:
+                pass
+            bodyin.write('\n'.join([
+                " {:12}ep={}  m={}  r={}  d={}  a1={}  a2={}  a3={}".format(file[:-5], ep, m, r, d, a1, a2, a3),
+                "  {}  {}  {}".format(c1, c2, c3),
+                "  {}  {}  {}".format(c4, c5, c6),
+                "  {}  {}  {}\n".format(Lx, Ly, Lz)]))
+
+        elif btype == "big":
+            if file == body_files[0]:
+                bodyin.write('\n'.join([
+                    ")O+_06 Big-body initial data  (WARNING: Do not delete this line!!)",
+                    ") Lines beginning with `)' are ignored.",
+                    ")---------------------------------------------------------------------",
+                    " style (Cartesian, Asteroidal, Cometary) = {}".format(coordinates),
+                    " epoch (in days) = {}".format(ep),
+                    ")---------------------------------------------------------------------\n"]))
+            else:
+                pass
+            bodyin.write('\n'.join([
+                " {:12}m={}  r={}  d={}  a1={}  a2={}  a3={}".format(file[:-5], m, r, d, a1, a2, a3),
+                "  {}  {}  {}".format(c1, c2, c3),
+                "  {}  {}  {}".format(c4, c5, c6),
+                "  {}  {}  {}\n".format(Lx, Ly, Lz)]))
+
+else:
     if btype == "small":
-        if file == body_files[0]:
-            bodyin.write('\n'.join([
-                ")O+_06 Small-body initial data  (WARNING: Do not delete this line!!)",
-                ") Lines beginning with `)' are ignored.",
-                ")---------------------------------------------------------------------",
-                " style (Cartesian, Asteroidal, Cometary) = {}".format(coordinates),
-                ")---------------------------------------------------------------------\n"]))
-        else:
-            pass
         bodyin.write('\n'.join([
-            " {:12}ep={}  m={}  r={}  d={}  a1={}  a2={}  a3={}".format(file[:-5], ep, m, r, d, a1, a2, a3),
-            "  {}  {}  {}".format(c1, c2, c3),
-            "  {}  {}  {}".format(c4, c5, c6),
-            "  {}  {}  {}\n".format(Lx, Ly, Lz)]))
-
+            ")O+_06 Small-body initial data  (WARNING: Do not delete this line!!)",
+            ") Lines beginning with `)' are ignored.",
+            ")---------------------------------------------------------------------",
+            " style (Cartesian, Asteroidal, Cometary) = Cartesian",
+            ")---------------------------------------------------------------------\n"]))
     elif btype == "big":
-        if file == body_files[0]:
-            bodyin.write('\n'.join([
-                ")O+_06 Big-body initial data  (WARNING: Do not delete this line!!)",
-                ") Lines beginning with `)' are ignored.",
-                ")---------------------------------------------------------------------",
-                " style (Cartesian, Asteroidal, Cometary) = {}".format(coordinates),
-                " epoch (in days) = {}".format(ep),
-                ")---------------------------------------------------------------------\n"]))
-        else:
-            pass
         bodyin.write('\n'.join([
-            " {:12}m={}  r={}  d={}  a1={}  a2={}  a3={}".format(file[:-5], m, r, d, a1, a2, a3),
-            "  {}  {}  {}".format(c1, c2, c3),
-            "  {}  {}  {}".format(c4, c5, c6),
-            "  {}  {}  {}\n".format(Lx, Ly, Lz)]))
+            ")O+_06 Big-body initial data  (WARNING: Do not delete this line!!)",
+            ") Lines beginning with `)' are ignored.",
+            ")---------------------------------------------------------------------",
+            " style (Cartesian, Asteroidal, Cometary) = Cartesian",
+            " epoch (in days) = 0",
+            ")---------------------------------------------------------------------\n"]))
+
 
 bodyin.close()
